fc7c41c0a632bf6a5e5426f1f37af898b5570ba1
Axi <axelandre91@gmail.com>
2023-01-30 23:36:36 -0300

    Added theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    // fontWeight: {
+    //         normal: "300",
+    //         medium: "600",
+    //         bold: "800",
+    //     },
+        colors: {
+            darkBlue: "#0B3954",
+            green: "#087E8B",
+            ligthBlue:"#BFD7EA",
+            orangeRed:"#FF5A5F",
+            darkRed:"#C81D25"
+        },
     extend: {},
   },
   plugins: [],
