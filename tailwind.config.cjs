fc51226277a5b210c4916fa75e4a0f876dc3130d
Vanessa Vun <vanessavun@gmail.com>
2023-01-30 16:01:36 -0800

    Added font family and colors in Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Quicksand', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    colors: {
+      'prussian-blue': '#0B3954',
+      'seaweed': '#087E8B',
+      'beau-blue': '#BFD7EA',
+      'orange': '#FF5A5F',
+      'red': '#C81D25'
+    }
   },
   plugins: [],
 };
