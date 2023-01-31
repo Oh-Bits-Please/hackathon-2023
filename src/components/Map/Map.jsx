9796531b716e16ef897cd8a4f518089162923118
Kailey <k.kotynski@gmail.com>
2023-01-31 13:57:46 -0800

    Added controlled map, dummy data for map markers, geolocation

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,18 +1,59 @@
 import * as React from "react";
-import Map from "react-map-gl";
+import Map, { Marker, GeolocateControl, Popup } from "react-map-gl";
+import { useState } from "react";
+import * as storeData from "/src/data/wholefoods.json";
 
 const MapBox = () => {
+  const [viewState, setViewState] = useState({
+    longitude: -122,
+    latitude: 47,
+    zoom: 10,
+    pitch: 50,
+  });
+
+  // const [selectedStore, setSelectedStore] = useState(null);
+
   return (
-    <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-      }}
-      style={{ width: 400, height: 400 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={import.meta.env.VITE_TOKEN}
-    />
+    <div>
+      <Map
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        style={{ width: 400, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        mapboxAccessToken={import.meta.env.VITE_TOKEN}
+      >
+        {storeData.default.map((store) => {
+          return (
+            <div>
+              <Marker
+                key={store.zip}
+                longitude={store.lng}
+                latitude={store.lat}
+              >
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedStore(store);
+                    // console.log(selectedStore);
+                  }}
+                >
+                  <img
+                    src="/src/images/map-marker.png"
+                    width={50}
+                    height={50}
+                  />
+                </button>
+              </Marker>
+            </div>
+          );
+        })}
+        {/* <Popup latitude={47} longitude={-122}>
+          <div>{selectedStore?.name || "test"}</div>
+        </Popup> */}
+
+        <GeolocateControl />
+      </Map>
+    </div>
   );
 };
 
