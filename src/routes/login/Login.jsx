70bf4310e54e6de0b4af81826763773b81c77e48
Vanessa Vun <vanessavun@gmail.com>
2023-02-01 16:40:17 -0800

    Moved Tailwind colors as extended, styled login page, edited overflow for menu

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 
 function Login() {
-
   return (
     <>
-      <h3>Login</h3>
-      <form>
-      <label>
-        <p>Email:</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password:</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Login now</button>
-        <p>Need an account? Sign up here.</p>
+      <div className="container w-screen text-center">
+        <h3 className="font-semibold text-xl m-4">Login</h3>
+        <form className="">
+          <p className="font-semibold">Email:</p>
+          <input
+            label="Email"
+            type="email"
+            name="email"
+            required
+            className="border border-gray-300 rounded-md w-80 h-8"
+          />
+          <p className="font-semibold">Password:</p>
+          <input
+            label="Password"
+            type="password"
+            name="password"
+            className="border border-gray-300 rounded-md w-80 h-8"
+          />
+          <div className="m-4">
+            <button
+              type="submit"
+              className="bg-darkBlue text-white font-bold w-72 h-12 text-lg rounded-lg"
+            >
+              Login now
+            </button>
+            <p className="font-semibold">Need an account? Sign up here.</p>
+          </div>
+        </form>
       </div>
-    </form>
     </>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,14 +7,15 @@ module.exports = {
     //         medium: "600",
     //         bold: "800",
     //     },
-        colors: {
-            darkBlue: "#0B3954",
-            green: "#087E8B",
-            lightBlue:"#BFD7EA",
-            orangeRed:"#FF5A5F",
-            darkRed:"#C81D25"
-        },
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#0B3954",
+        green: "#087E8B",
+        lightBlue: "#BFD7EA",
+        orangeRed: "#FF5A5F",
+        darkRed: "#C81D25",
+      },
+    },
   },
   plugins: [],
 };
