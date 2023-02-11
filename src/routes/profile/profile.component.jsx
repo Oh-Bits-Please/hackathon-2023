cd4e6122840d817aa8a3b86f1d92762b2ec07773
Axi <axelandre91@gmail.com>
2023-02-11 11:16:38 -0300

    Changed styles

--- src/routes/profile/profile.component.jsx
+++ src/routes/profile/profile.component.jsx
@@ -30,25 +30,29 @@ function Profile() {
   ];
 
   return (
-    <div>
-      <h1 className="w-96 text-2xl font-bold p-4 md:text-4xl md:w-3/6 lg:w-2/5">Profile</h1>
-      <div className='bg-orangeRed m-4 rounded-md text-sm'>
-        <div className='p-2 flex'>
-          <img src={AvatarPicture} className='w-40' alt="" />
-          <div className='flex flex-wrap text-white'>
-            <h2 className='flex items-center'><HiUser className='mr-1' />Name: Jane Doe</h2>
-            <p className='flex items-center'><HiEnvelope className='mr-1' />Email: jane_doe@gmail.com</p>
-            <p className='flex items-center'><HiMapPin className='mr-1' />Address: 1234 Dortha St</p>
-            <p className='flex items-center'><HiPhone className='mr-1' />Phone number: (801) 345-55-5555</p>
-            <a className=" text-darkBlue font-bold rounded-lg text-center underline hover:text-darkRed cursor-pointer">Change password</a>
+    <div className='md:flex md:gap-8 md:columns-2 '>
+      <div>
+        <h1 className="w-96 text-2xl font-bold p-4 md:text-4xl md:w-3/6 lg:w-2/5">Profile</h1>
+        <div className='bg-orangeRed m-4 rounded-md text-sm'>
+          <div className='p-2 flex'>
+            <img src={AvatarPicture} className='w-40' alt="" />
+            <div className='flex flex-wrap text-white'>
+              <h2 className='flex items-center'><HiUser className='mr-1' />Name: Jane Doe</h2>
+              <p className='flex items-center'><HiEnvelope className='mr-1' />Email: jane_doe@gmail.com</p>
+              <p className='flex items-center'><HiMapPin className='mr-1' />Address: 1234 Dortha St</p>
+              <p className='flex items-center'><HiPhone className='mr-1' />Phone number: (801) 345-55-5555</p>
+              <a className=" text-darkBlue font-bold rounded-lg text-center underline hover:text-darkRed cursor-pointer">Change password</a>
+            </div>
           </div>
         </div>
       </div>
-      <div className='flex items-center'>
-        <h1 className=" text-2xl font-bold ml-4 py-4 md:text-4xl md:w-3/6 lg:w-2/5">Favorites </h1>
-        <HiHeart className='pl-2 text-red text-3xl' />
+      <div>
+        <div className='flex items-center w-100'>
+          <h1 className=" text-2xl font-bold ml-4 py-4 md:text-4xl md:w-3/6 lg:w-2/5">Favorites </h1>
+          <HiHeart className='pl-2 text-red text-3xl' />
+        </div>
+        <ProductList />
       </div>
-      <ProductList />
     </div>
   )
 }
