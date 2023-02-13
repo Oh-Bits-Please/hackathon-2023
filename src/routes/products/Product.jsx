62d884f6f9b26a039affcc313451c59612e03591
Vanessa Vun <vanessavun@gmail.com>
2023-02-13 13:23:11 -0800

    Edited typo in Product component

--- src/routes/products/Product.jsx
+++ src/routes/products/Product.jsx
@@ -18,7 +18,7 @@ const Product = ({ id, name, price, discount, img, location, lat, lng }) => {
         <div className="mx-2 w-full flex flex-col align-middle justify-center">
           <div className="flex justify-between">
             <h1 className="font-bold text-lg">{name}</h1>
-          </div>git
+          </div>
           <p>{price}</p>
           <p>Location: {location ? location : "Not found"}</p>
         </div>
