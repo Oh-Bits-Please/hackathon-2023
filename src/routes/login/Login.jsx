2fd659ff7ddba6326784abfb70521a477ea9640e
Vanessa Vun <vanessavun@gmail.com>
2023-02-13 15:09:34 -0800

    Edited login page to remove black color and edited signup page to remove log out button

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -29,9 +29,9 @@ function Login() {
 
   return (
     <>
-      <section className="bg-gray-50">
+      <section className="bg-lightGray">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow border border-lightGray md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 ">
+          <div className="w-full bg-white rounded-lg shadow border border-lightGray md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-darkRed md:text-2xl">
                 Welcome back
@@ -39,18 +39,18 @@ function Login() {
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-semibold text-gray-900">Your email</label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-lightGray text-gray-900 sm:text-sm rounded-lg focus:ring-darkRed  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:border-darkRed"
+                  <input type="email" name="email" id="email" className="bg-gray-50 border border-lightGray text-gray-900 sm:text-sm rounded-lg focus:ring-darkRed  block w-full p-2.5 placeholder-gray-400 focus:border-darkRed"
                     onChange={(e) => setLoginEmail(e.target.value)} placeholder="youremail@domain.com" required="" />
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-semibold text-gray-900">Password</label>
                   <input type="password" name="password" id="password"
-                    onChange={(e) => setLoginPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-lightGray text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-darkRed focus:border-darkRed" required="" />
+                    onChange={(e) => setLoginPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-lightGray text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 placeholder-gray-400 focus:ring-darkRed focus:border-darkRed" required="" />
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-lightGray rounded bg-gray-50 focus:ring-3  dark:bg-gray-700 border-gray-600 focus:ring-darkRed ring-offset-gray-800" required="" />
+                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-lightGray rounded bg-gray-50 focus:ring-3  dark:bg-gray-700 focus:ring-darkRed ring-offset-gray-800" required="" />
                     </div>
                     <div className="ml-3 text-sm">
                       <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
